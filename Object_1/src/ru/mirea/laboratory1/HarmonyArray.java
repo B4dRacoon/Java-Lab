899f4f1f35a8c20f951f61373b2ece70c1ca344a
B4dRacoon <90094097+B4dRacoon@users.noreply.github.com>
2021-09-10 21:21:52 +0300

    Add files via upload

---
+++ Object_1/src/ru/mirea/laboratory1/HarmonyArray.java
@@ -0,0 +1,9 @@
+package ru.mirea.laboratory1;
+
+public class HarmonyArray {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++){
+            System.out.println(1./i);
+        }
+    }
+}
